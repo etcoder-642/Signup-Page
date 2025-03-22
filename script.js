3f44b47aec4f11534919876785ab10060478acd0
etcoder-642 <manassehsamuel642@gmail.com>
2025-03-22 10:59:33 +0300

    1. Added client-side validation using HTML and javascript for all input types 2. Added some CSS functionalities

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const passwrd = document.querySelector('#password');
-const confirmPassword = document.querySelector('#confirmPassword');
+document.addEventListener('input', (e)=>{
+    if(!e.target.checkValidity()){
+        e.target.style.border = "3px solid red"
+    }
+     else {
+        e.target.style.border = "none";        
+    }
+})
+
+let passwrd = document.querySelector('#password');
+let confirmPassword = document.querySelector('#confirmPassword');
+
+document.querySelector('.btn').onclick = ()=>{
+    let result = (!(passwrd.value == confirmPassword.value)) ? "PassWord didn't Matched":"";
+    console.log(result.length, result)
+    if(result.length >= 1)
+        alert(result);
+}
+
+// setInterval(()=>{
+//     if(passwrd.value == confirmPassword.value)
+//         console.log(passwrd.value, confirmPassword.value, "Yeah, you made it!")
+// }, 10000)
+
 
